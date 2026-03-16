fb27077f22a0314354963b9542df3d07d9e2c27c
Kenny Higgins <terrestrialabyss@gmail.com>
2026-03-16 09:06:25 -0400

    added all resources in the resources folder (except the structure/tree.bin) to the assetManager

--- src/gameLayer/assetManager.cpp
+++ src/gameLayer/assetManager.cpp
@@ -4,12 +4,35 @@ namespace GameLayer {
 
 void AssetManager::loadAll() {
 #pragma region Player sprites
-    player        = LoadTexture(RESOURCES_PATH "player.png");
-    playerSprites = LoadTexture(RESOURCES_PATH "playerSprites.png");
-    items         = LoadTexture(RESOURCES_PATH "items.png");
-    hearts        = LoadTexture(RESOURCES_PATH "hearts.png");
-
-    frame = LoadTexture(RESOURCES_PATH "frame.png");
+    player          = LoadTexture(RESOURCES_PATH "player.png");
+    playerSprites   = LoadTexture(RESOURCES_PATH "playerSprites.png");
+    items           = LoadTexture(RESOURCES_PATH "items.png");
+    hearts          = LoadTexture(RESOURCES_PATH "hearts.png");
+    copperArmorBack = LoadTexture(RESOURCES_PATH "body/copper_armour_back.png");
+    copperArmorFeet = LoadTexture(RESOURCES_PATH "body/copper_armour_feet.png");
+    copperArmorFront =
+        LoadTexture(RESOURCES_PATH "body/copper_armour_front.png");
+    copperArmorFeet = LoadTexture(RESOURCES_PATH "body/copper_armour_head.png");
+    goldArmorBack   = LoadTexture(RESOURCES_PATH "body/gold_armour_back.png");
+    goldArmorFeet   = LoadTexture(RESOURCES_PATH "body/gold_armour_feet.png");
+    goldArmorFront  = LoadTexture(RESOURCES_PATH "body/gold_armour_front.png");
+    goldArmorHead   = LoadTexture(RESOURCES_PATH "body/gold_armour_head.png");
+    iceArmorBack    = LoadTexture(RESOURCES_PATH "body/ice_armour_back.png");
+    iceArmorFeet    = LoadTexture(RESOURCES_PATH "body/ice_armour_feet.png");
+    iceArmorFront   = LoadTexture(RESOURCES_PATH "body/ice_armour_front.png");
+    iceArmorHead    = LoadTexture(RESOURCES_PATH "body/ice_armour_head.png");
+    ironArmorBack   = LoadTexture(RESOURCES_PATH "body/iron_armour_back.png");
+    ironArmorFeet   = LoadTexture(RESOURCES_PATH "body/iron_armour_feet.png");
+    ironArmorFront  = LoadTexture(RESOURCES_PATH "body/iron_armour_front.png");
+    ironArmorHead   = LoadTexture(RESOURCES_PATH "body/iron_armour_head.png");
+    partyHat        = LoadTexture(RESOURCES_PATH "body/party_hat");
+    playerBack      = LoadTexture(RESOURCES_PATH "body/player_back.png");
+    playerFeet      = LoadTexture(RESOURCES_PATH "body/player_feet.png");
+    playerFront     = LoadTexture(RESOURCES_PATH "body/player_front.png");
+    playerHead      = LoadTexture(RESOURCES_PATH "body/player_head.png");
+    sunglasses      = LoadTexture(RESOURCES_PATH "body/sunglasses.png");
+    thumbs          = LoadTexture(RESOURCES_PATH "body/Thumbs.png");
+    frame           = LoadTexture(RESOURCES_PATH "frame.png");
 #pragma endregion
 #pragma region Environment sprites
     dirt     = LoadTexture(RESOURCES_PATH "dirt.png");
@@ -31,7 +54,47 @@ void AssetManager::loadAll() {
     smallBlueSlime = LoadTexture(RESOURCES_PATH "smallblueslime.png");
     zombieEskimo   = LoadTexture(RESOURCES_PATH "zombie_eskimo.png");
     zombie         = LoadTexture(RESOURCES_PATH "zombie.png");
+#pragma endregion
 
+#pragma region Music
+    bonus   = LoadMusicStream(RESOURCES_PATH "/music/bonus.ogg");
+    cave    = LoadMusicStream(RESOURCES_PATH "/music/cave.ogg");
+    credits = LoadMusicStream(RESOURCES_PATH "/music/Credits.ogg");
+    desert  = LoadMusicStream(RESOURCES_PATH "/music/desert.ogg");
+    forest  = LoadMusicStream(RESOURCES_PATH "/music/forest.ogg");
+    snow    = LoadMusicStream(RESOURCES_PATH "/music/snow.ogg");
+#pragma endregion
+#pragma region Sound
+    breakSound  = LoadSound(RESOURCES_PATH "sounds/break.ogg");
+    place       = LoadSound(RESOURCES_PATH "sounds/place.ogg");
+    backButton  = LoadSound(RESOURCES_PATH "sounds/bonus/BackButton.ogg");
+    buttonPress = LoadSound(RESOURCES_PATH "sounds/bonus/ButtonPress.ogg");
+    checkBoxOff = LoadSound(RESOURCES_PATH "sounds/bonus/CheckBoxOff.ogg");
+    checkBoxOn  = LoadSound(RESOURCES_PATH "sounds/bonus/CheckBoxOn.ogg");
+    finalHit1   = LoadSound(RESOURCES_PATH "sounds/bonus/FinalHit1.ogg");
+    finalHit2   = LoadSound(RESOURCES_PATH "sounds/bonus/FinalHit2.ogg");
+    finalHit3   = LoadSound(RESOURCES_PATH "sounds/bonus/FinalHit3.ogg");
+    hImpact1    = LoadSound(RESOURCES_PATH "sounds/bonus/HImpact1.ogg");
+    hImpact2    = LoadSound(RESOURCES_PATH "sounds/bonus/HImpact2.ogg");
+    hImpact3    = LoadSound(RESOURCES_PATH "sounds/bonus/HImpact3.ogg");
+    hit1        = LoadSound(RESOURCES_PATH "sounds/bonus/Hit1.ogg");
+    hit2        = LoadSound(RESOURCES_PATH "sounds/bonus/Hit2.ogg");
+    hit3        = LoadSound(RESOURCES_PATH "sounds/bonus/Hit3.ogg");
+    slider      = LoadSound(RESOURCES_PATH "sounds/bonus/Slider.ogg");
+    stone1      = LoadSound(RESOURCES_PATH "sounds/bonus/Stone1.ogg");
+    stone2      = LoadSound(RESOURCES_PATH "sounds/bonus/Stone2.ogg");
+    stone3      = LoadSound(RESOURCES_PATH "sounds/bonus/Stone3.ogg");
+    stone4      = LoadSound(RESOURCES_PATH "sounds/bonus/Stone4.ogg");
+    stone5      = LoadSound(RESOURCES_PATH "sounds/bonus/Stone5.ogg");
+    stone6      = LoadSound(RESOURCES_PATH "sounds/bonus/Stone6.ogg");
+    stone7      = LoadSound(RESOURCES_PATH "sounds/bonus/Stone7.ogg");
+    wood1       = LoadSound(RESOURCES_PATH "sounds/bonus/Wood1.ogg");
+    wood2       = LoadSound(RESOURCES_PATH "sounds/bonus/Wood2.ogg");
+    wood3       = LoadSound(RESOURCES_PATH "sounds/bonus/Wood3.ogg");
+    wood4       = LoadSound(RESOURCES_PATH "sounds/bonus/Wood4.ogg");
+    wood5       = LoadSound(RESOURCES_PATH "sounds/bonus/Wood5.ogg");
+    wood6       = LoadSound(RESOURCES_PATH "sounds/bonus/Wood6.ogg");
+    wood7       = LoadSound(RESOURCES_PATH "sounds/bonus/Wood7.ogg");
 #pragma endregion
 }
 
--- src/gameLayer/assetManager.h
+++ src/gameLayer/assetManager.h
@@ -5,10 +5,33 @@ namespace GameLayer {
 
 struct AssetManager {
 #pragma region Player sprites
-    Texture2D player        = {};
-    Texture2D playerSprites = {};
-    Texture2D items         = {};
-    Texture2D hearts        = {};
+    Texture2D player           = {};
+    Texture2D playerSprites    = {};
+    Texture2D items            = {};
+    Texture2D hearts           = {};
+    Texture2D copperArmorBack  = {};
+    Texture2D copperArmorFeet  = {};
+    Texture2D copperArmorFront = {};
+    Texture2D copperArmorHead  = {};
+    Texture2D goldArmorBack    = {};
+    Texture2D goldArmorFeet    = {};
+    Texture2D goldArmorFront   = {};
+    Texture2D goldArmorHead    = {};
+    Texture2D iceArmorBack     = {};
+    Texture2D iceArmorFeet     = {};
+    Texture2D iceArmorFront    = {};
+    Texture2D iceArmorHead     = {};
+    Texture2D ironArmorBack    = {};
+    Texture2D ironArmorFeet    = {};
+    Texture2D ironArmorFront   = {};
+    Texture2D ironArmorHead    = {};
+    Texture2D partyHat         = {};
+    Texture2D playerBack       = {};
+    Texture2D playerFeet       = {};
+    Texture2D playerFront      = {};
+    Texture2D playerHead       = {};
+    Texture2D sunglasses       = {};
+    Texture2D thumbs           = {};
 
     Texture2D frame = {};
 #pragma endregion
@@ -31,6 +54,48 @@ struct AssetManager {
     Texture2D smallBlueSlime = {};
     Texture2D zombieEskimo   = {};
     Texture2D zombie         = {};
+#pragma endregion
+
+#pragma region Music
+    Music bonus   = {};
+    Music cave    = {};
+    Music credits = {};
+    Music desert  = {};
+    Music forest  = {};
+    Music snow    = {};
+#pragma endregion
+
+#pragma region Sounds
+    Sound breakSound  = {};
+    Sound place       = {};
+    Sound backButton  = {};
+    Sound buttonPress = {};
+    Sound checkBoxOff = {};
+    Sound checkBoxOn  = {};
+    Sound finalHit1   = {};
+    Sound finalHit2   = {};
+    Sound finalHit3   = {};
+    Sound hImpact1    = {};
+    Sound hImpact2    = {};
+    Sound hImpact3    = {};
+    Sound hit1        = {};
+    Sound hit2        = {};
+    Sound hit3        = {};
+    Sound slider      = {};
+    Sound stone1      = {};
+    Sound stone2      = {};
+    Sound stone3      = {};
+    Sound stone4      = {};
+    Sound stone5      = {};
+    Sound stone6      = {};
+    Sound stone7      = {};
+    Sound wood1       = {};
+    Sound wood2       = {};
+    Sound wood3       = {};
+    Sound wood4       = {};
+    Sound wood5       = {};
+    Sound wood6       = {};
+    Sound wood7       = {};
 #pragma endregion
 
     void loadAll();
